c22d247494f9a2d0595496217d99fd1be33c30e3
Andrew Rueckert <ruke47@users.noreply.github.com>
2022-12-02 21:40:21 -0800

    clean up day 03

--- 03/main.py
+++ 03/main.py
@@ -7,45 +7,46 @@ def gen_alpha_scores():
         scores[letter.upper()] = idx + 26
     return scores
 
+def part1(packs, scores):
+    score = 0
+    for items in packs:
+        if len(items) % 2 != 0:
+            print(f"ERROR: got non-even number of items {len(items), items}")
+
+        half = int(len(items) / 2)
+        unique_left = set(items[:half])
+        unique_right = set(items[half:])
+        overlap = unique_right.intersection(unique_left)
+        if len(overlap) != 1:
+            print(f"ERROR: got multiple overlap {overlap} in {(left, right)}")
+
+        score += scores[overlap.pop()]
+    print(f"Score 1: {score}")
+
+def part2(packs, scores):
+    score = 0
+    for idx in range(0, len(packs), 3):
+        elf1 = set(packs[idx])
+        elf2 = set(packs[idx + 1])
+        elf3 = set(packs[idx + 2])
+        shared = elf1.intersection(elf2).intersection(elf3)
+        if len(shared) != 1:
+            print(f"ERROR: didn't get 1 shared between {(elf1, elf2, elf3)}")
+
+        score += scores[shared.pop()]
+    print(f"Score 2: {score}")
+
 
 def main():
     packs = []
     with open("input") as file:
         for line in file:
-            items = line.strip()
-            if len(items) % 2 != 0:
-                print(f"ERROR: got non-even number of items {len(items), items}")
-
-            half = int(len(items) / 2)
-            left = items[:half]
-            right = items[half:]
-
-            unique_left = set(left)
-            unique_right = set(right)
-            unique = set(items)
-            overlap = unique_right.intersection(unique_left)
-            if len(overlap) != 1:
-                print(f"ERROR: got multiple overlap {overlap} in {(left, right)}")
-
-            packs.append((unique, overlap.pop()))
+            packs.append(line.strip())
 
     scores = gen_alpha_scores()
 
-    score = sum([scores[overlap] for (unique, overlap) in packs])
-    print(f"Sum Scores 1: {score}")
-
-    score2 = 0
-    for i in range(0, len(packs), 3):
-        elf1 = packs[i][0]
-        elf2 = packs[i + 1][0]
-        elf3 = packs[i + 2][0]
-
-        shared = elf1.intersection(elf2).intersection(elf3)
-        if len(shared) != 1:
-            print(f"ERROR: didn't get 1 shared between {(elf1, elf2, elf3)}")
-
-        score2 += scores[shared.pop()]
-    print(f"Score 2: {score2}")
+    part1(packs, scores)
+    part2(packs, scores)
 
 
 if __name__ == '__main__':
