8f10c4dafee520cd94c0c004ba9766d29a253489
Andrew Rueckert <ruke47@users.noreply.github.com>
2022-12-08 21:48:22 -0800

    Day 09 part 2
    not as bad as I thought it'd be!

--- 09/main.py
+++ 09/main.py
@@ -13,10 +13,9 @@ def normalize(val):
 
 
 class Grid:
-    def __init__(self, moves):
+    def __init__(self, moves, length):
         self.moves = moves
-        self.head = (0, 0)
-        self.tail = (0, 0)
+        self.knots = [(0, 0) for i in range(length)]
         self.tail_positions = [(0, 0)]
 
     def run(self):
@@ -28,12 +27,15 @@ class Grid:
         dir_offset = dir_offsets[direction]
 
         for i in range(count):
-            self.head = (self.head[0] + dir_offset[0], self.head[1] + dir_offset[1])
-            self.catch_up_tail()
+            head_x, head_y = self.knots[0]
+            self.knots[0] = (head_x + dir_offset[0], head_y + dir_offset[1])
+            for i in range(0, len(self.knots) - 1):
+                self.catch_up_tail(i, i + 1)
 
-    def catch_up_tail(self):
-        hx, hy = self.head
-        tx, ty = self.tail
+    # head is the dragger, tail is the dragee (not necessarily the final knot in the rope)
+    def catch_up_tail(self, head_pos, tail_pos):
+        hx, hy = self.knots[head_pos]
+        tx, ty = self.knots[tail_pos]
         offset_x = hx - tx
         offset_y = hy - ty
 
@@ -44,13 +46,15 @@ class Grid:
         normalized_y = normalize(offset_y)
 
         if offset_x == 0:
-            self.tail = (tx, ty + normalized_y)
+            self.knots[tail_pos] = (tx, ty + normalized_y)
         elif offset_y == 0:
-            self.tail = (tx + normalized_x, ty)
+            self.knots[tail_pos] = (tx + normalized_x, ty)
         else:
-            self.tail = (tx + normalized_x, ty + normalized_y)
+            self.knots[tail_pos] = (tx + normalized_x, ty + normalized_y)
 
-        self.tail_positions.append(self.tail)
+        # if this is the last knot, add its position to the tail positions
+        if tail_pos == len(self.knots) - 1:
+            self.tail_positions.append(self.knots[tail_pos])
 
 
 def main():
@@ -60,10 +64,13 @@ def main():
             line_parts = line.strip().split()
             head_moves.append((line_parts[0], int(line_parts[1])))
 
-    grid = Grid(head_moves)
-    grid.run()
-    print(len(set(grid.tail_positions)))
+    grid1 = Grid(head_moves, 2)
+    grid1.run()
+    print(len(set(grid1.tail_positions)))
 
+    grid2 = Grid(head_moves, 10)
+    grid2.run()
+    print(len(set(grid2.tail_positions)))
 
 if __name__ == '__main__':
     main()
