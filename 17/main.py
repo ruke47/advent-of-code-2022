decc1e70ba95d0be5eb7e9db0824a0c7f8be040a
Andrew Rueckert <ruke47@users.noreply.github.com>
2022-12-17 00:13:18 -0800

    Incomplete 17.2
    Works for the example problem, but produces the wrong output for my full input. Very frustrating!

--- 17/main.py
+++ 17/main.py
@@ -1,6 +1,7 @@
-from itertools import cycle, repeat
+from itertools import cycle
 from collections.abc import Iterable, Iterator
-
+from math import floor
+from time import time
 
 class Shape:
     def __init__(self, points: list[(int, int)], read_only=False):
@@ -54,17 +55,58 @@ class Shape:
 
 
 class Grid:
-    def __init__(self, width: int):
+    def __init__(self, width: int, wind_length):
+        self.width = width
+        self.wind_length = wind_length
         self.min_y = 0
         self.max_y = -1
         self.min_x = 0
         self.max_x = width - 1
         self.obstructions = set()
+        self.column_rel_heights = {}
+        self.cycle_id = -1
+        self.all_heights = []
+
+    def height(self):
+        return self.max_y + 1
+
+    def drop_shape(self, shape_template: Shape, wind: Iterator[str]):
+        self.cycle_id += 1
+        cur_shape = shape_template.__copy__()
+        cur_shape.initialize(self)
+        while True:
+            cur_shape.move_side(next(wind), self)
+            if not cur_shape.move_down(self):
+                self.land(cur_shape)
+                self.all_heights.append(self.height())
+                break
 
     def land(self, shape: Shape):
         for point in shape.points:
             self.max_y = max(self.max_y, max(y for x, y in shape.points))
             self.obstructions.add(point)
+
+    def check_relative_heights(self):
+        highest_point = {x: -1 for x in range(0, self.width)}
+        for x, y in self.obstructions:
+            highest_point[x] = max(highest_point[x], y)
+        normalized_floor = min(highest_point.values())
+
+        if self.cycle_id % self.wind_length == 0:
+            normalized_heights = []
+            for x in range(0, self.width):
+                normalized_heights.append(highest_point[x] - normalized_floor)
+            normalized_heights = tuple(normalized_heights)
+
+            if normalized_heights in self.column_rel_heights:
+                return self.column_rel_heights[normalized_heights]
+            else:
+                self.column_rel_heights[normalized_heights] = (self.cycle_id, self.height())
+                return None
+        elif self.cycle_id % 1000 == 0:
+            # discard everything beneath the highest point in each column
+            self.obstructions = set((x, y) for x, y in self.obstructions if y >= normalized_floor)
+        return None
 
 
 def load_shapes() -> Iterable[Shape]:
@@ -83,29 +125,55 @@ def load_wind() -> (Iterator[str], int):
         return cycle(wind_loop), len(wind_loop)
 
 
-def part1():
-    grid = Grid(7)
+def part1(cutoff: int):
     wind, wind_len = load_wind()
+    grid = Grid(7, wind_len)
     for drop_num, shape_template in enumerate(load_shapes()):
-        if drop_num == 2022:
+        if drop_num == cutoff:
             break
-        cur_shape = shape_template.__copy__()
-        cur_shape.initialize(grid)
-        while True:
-            cur_shape.move_side(next(wind), grid)
-            if not cur_shape.move_down(grid):
-                grid.land(cur_shape)
-                break
-    print(grid.max_y + 1)
+        grid.drop_shape(shape_template, wind)
+    return grid
 
 
 def part2():
-    pass
+    mega_cycles = 1_000_000_000_000
+    wind, wind_len = load_wind()
+    grid = Grid(7, wind_len)
+    for drop_num, shape_template in enumerate(load_shapes()):
+        grid.drop_shape(shape_template, wind)
+        if drop_num % 1000 != 0 and drop_num % wind_len != 0:
+            continue
+        loop_state = grid.check_relative_heights()
+        if loop_state:
+            loop_begin_idx, before_loop_height = loop_state
+            loop_height = grid.height() - before_loop_height
+            loop_length = drop_num - loop_begin_idx
+
+            cycles_remaining = floor((mega_cycles - drop_num - 1) / loop_length)
+            remaining_cycle_height = cycles_remaining * loop_height
+
+            leftover_cycles = (mega_cycles - drop_num - 1) % loop_length
+            leftover_cycle_height = grid.all_heights[loop_begin_idx + leftover_cycles] - before_loop_height
+
+            total_height = grid.height() + remaining_cycle_height + leftover_cycle_height
+            print(f"Loop on cycles {loop_begin_idx} - {drop_num}. Loop will repeat {cycles_remaining} more times.")
+            print(f"\tBefore-loop height: {before_loop_height}")
+            print(f"\tLoop-height: {loop_height}")
+            print(f"\tLoop-length: {loop_length}")
+            print(f"\tLeftover cycles: {leftover_cycles}")
+            print(f"\tLeftover Height: {leftover_cycle_height}")
+            print(f"Total Height: {total_height}")
+            break
+
+
 
 
 def main():
-    part1()
+    print(part1(2022).height())
+    begin = time()
     part2()
+    time_took = time() - begin
+    print(f"Took {floor(time_took/60)}m {floor(time_took) % 60}s")
 
 
 if __name__ == '__main__':
