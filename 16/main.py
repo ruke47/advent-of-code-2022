529967cbc7361807a800fef2ee06f0609034b9d6
Andrew Rueckert <ruke47@users.noreply.github.com>
2022-12-16 18:57:52 -0800

    More work on 16.
    For each position, consider whether we've been in this position before, but with more time left and more valves open. If so, don't bother exploring from here.
    
    I think it's a coincidence; this fails for the example, but works for the input. I think the example has a weird case where I'm discarding a 3-valve, fewer-points state to a 4-valve, more-points state, but the 3-valve state would overtake the 4-valve state by opening another valve. If that makes sense.

--- 16/main.py
+++ 16/main.py
@@ -1,21 +1,27 @@
 import re
-from itertools import permutations
+from collections.abc import Iterable
 
 input_p = re.compile(r"Valve (\w+) has flow rate=(\d+); tunnel[s]? lead[s]? to valve[s]? (.*)")
-
+tap = "🚰"
 
 class Valve:
-    def __init__(self, name, flow_rate, neighbors):
+    next_valve_id = 1
+
+    def __init__(self, name: str, flow_rate: int, neighbors: list[str]):
         self.name = name
         self.flow_rate = flow_rate
         self.neighbors_names = neighbors
         self.neighbors = None
         self.distance_to = {}
+        self.valve_id = 0
+        if flow_rate > 0:
+            self.valve_id = Valve.next_valve_id
+            Valve.next_valve_id *= 2
 
-    def find_neighbors(self, valves):
+    def find_neighbors(self, valves: dict[str, "Valve"]):
         self.neighbors = [valves[name] for name in self.neighbors_names]
 
-    def find_distance_to_others(self, valves):
+    def find_distance_to_others(self, valves: Iterable["Valve"]):
         for valve in valves:
             if valve == self:
                 self.distance_to[self.name] = 0
@@ -32,7 +38,7 @@ class Valve:
 
 
 class Node:
-    def __init__(self, valve):
+    def __init__(self, valve: Valve):
         self.valve = valve
         self.distance = 100
 
@@ -43,7 +49,7 @@ class Node:
         self.__str__()
 
 
-def find_distance(start, end, all_valves):
+def find_distance(start: Valve, end: Valve, all_valves: Iterable[Valve]):
     nodes = {valve.name: Node(valve) for valve in all_valves}
     nodes[start.name].distance = 0
     unvisited = set(nodes.values())
@@ -76,29 +82,103 @@ def read_input():
     return valves
 
 
-def rush_valves(start_node, unopened_valves, time):
-    valve_orders = permutations(unopened_valves)
-    max_score = 0
-    for i, order in enumerate(valve_orders):
-        if i % 1_000_000 == 0:
-            print(f"\t - {i}")
-        score = 0
-        cur_node = start_node
-        time_left = time
-        for valve in order:
-            time_cost = cur_node.distance_to[valve.name] + 1
-            time_left -= time_cost
-            score += valve.flow_rate * time_left
-            cur_node = valve
-        max_score = max(max_score, score)
-    return max_score
+class GameState:
+    def __init__(self, position: Valve, valves_enabled: int, time_left: int, score: int, history=[], is_move_action=True):
+        self.position = position
+        self.valves_enabled = valves_enabled
+        self.time_left = time_left
+        self.score = score
+        self.history = history.copy()
+        if is_move_action:
+            self.history.append(position.name)
+        else:
+            self.history.append(tap)
+
+    def __str__(self):
+        return f"[{self.position.name}]({self.time_left} - {self.valves_enabled}]: {self.score}";
+
+    def __repr__(self):
+        return self.__str__()
+
+    def is_better_than(self, other: "GameState"):
+        i_have_equal_valves = self.valves_enabled & other.valves_enabled == other.valves_enabled
+        # i_have_equal_vales = self.valves_enabled == other.valves_enabled
+        if i_have_equal_valves and self.time_left >= other.time_left and self.score >= other.score:
+            return True
+        return False
+
+    def already_opened_valve(self):
+        if self.position.flow_rate == 0:
+            return True
+        if self.position.valve_id & self.valves_enabled == self.position.valve_id:
+            return True
+        return False
+
+    def state_with_valve_open(self):
+        new_enabled = self.valves_enabled | self.position.valve_id
+        new_time_left = self.time_left - 1
+        new_score = new_time_left * self.position.flow_rate + self.score
+        if new_time_left > 0:
+            return GameState(self.position, new_enabled, new_time_left, new_score, self.history, is_move_action=False)
+        else:
+            return None
+
+    def state_with_move_to(self, neighbor: Valve):
+        if neighbor not in self.position.neighbors:
+            raise f"Cannot move from {self.position} to {neighbor}"
+        if self.valves_enabled == Valve.next_valve_id - 1:
+            # if all valves are open, stop running around
+            return None
+        if self.time_left > 1:
+            return GameState(neighbor, self.valves_enabled, self.time_left - 1, self.score, self.history)
+        else:
+            return None
+
+
+def no_better_positions(new_state: GameState, good_position_map: dict[str, list[GameState]]):
+    good_positions = good_position_map[new_state.position.name]
+    if any(other.is_better_than(new_state) for other in good_positions):
+        return False
+    else:
+        not_worse_positions = [position for position in good_positions if not new_state.is_better_than(position)]
+        not_worse_positions.append(new_state)
+        good_position_map[new_state.position.name] = not_worse_positions
+        return True
+
+
+def explore_map(start_node: Valve, valve_map: dict[str, Valve], time: int):
+    seen_positions = {valve.name: [] for valve in valve_map.values()}
+    initial_state = GameState(start_node, 0, time, 0)
+    seen_positions[start_node.name].append(initial_state)
+    to_explore = [initial_state]
+    comparisons = 0
+    while to_explore:
+        comparisons += 1
+        cur_state = to_explore.pop()
+        for neighbor_position in cur_state.position.neighbors:
+            new_state = cur_state.state_with_move_to(neighbor_position)
+            if new_state and no_better_positions(new_state, seen_positions):
+                to_explore.append(new_state)
+        if not cur_state.already_opened_valve():
+            new_state = cur_state.state_with_valve_open()
+            if new_state and no_better_positions(new_state, seen_positions):
+                to_explore.append(new_state)
+    best_position = initial_state
+    for position_list in seen_positions.values():
+        for position in position_list:
+            best_position = max(best_position, position, key=lambda state: state.score)
+
+    return best_position.score
 
 
 def part1():
     valves = read_input()
+    # TODO: start with a stack of [(starting_position, enabled_faucets)]
+    # pop a reached-state off of the stack, and for each neighbor, consider whether you've been
+    # in that (location, enabled-faucets|potential-points) before, but had more time left. Only if you have not,
+    # push that next-state onto the stack.
     entry = valves['AA']
-    unopened_valves = set(valve for valve in valves.values() if valve.flow_rate > 0)
-    score = rush_valves(entry, unopened_valves, 30)
+    score = explore_map(entry, valves, 30)
     print(score)
 
 
