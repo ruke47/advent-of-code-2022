c6d13efa0b02b0435db2d7bdaf67d2feb20b9732
Andrew Rueckert <ruke47@users.noreply.github.com>
2022-12-19 23:31:16 -0800

    Day 20.1
    You DO remove the number while shifting around. >:(

--- 20/main.py
+++ 20/main.py
@@ -36,7 +36,11 @@ def load():
 def part1():
     original_idx, zero = load()
     length = len(original_idx)
+    print_array(zero)
+    print_array_backwards(zero)
     for i in range(length):
+        if i % 100 == 0:
+            print(f"\t- {i}/{length}")
         cur = original_idx[i]
         new_place = cur
         val = cur.value
@@ -45,47 +49,82 @@ def part1():
         if val > 0:
             while val > 0:
                 new_place = new_place.next
+                if new_place == cur:
+                    new_place = new_place.next
                 val -= 1
+            if cur == new_place or new_place.next == cur:
+                continue
         elif val < 0:
             while val < 0:
                 new_place = new_place.prior
+                if new_place == cur:
+                    new_place = new_place.prior
                 val += 1
             # move back 1 more so it's the same as if we'd come at this from the front
             new_place = new_place.prior
+            if cur == new_place or new_place.next == cur:
+                continue
         # use temp variables to avoid gunking up state
-        cur_next_prior = cur.prior
-        cur_prior_next = cur.next
-        cur_prior = new_place.prior
-        cur_next = new_place.next
+        out_next = cur.next
+        out_prior = cur.prior
 
-        cur.next.prior = cur_next_prior
-        cur.prior.next = cur_prior_next
-        new_place.next.prior = cur
-        new_place.next = cur
-        cur.prior = cur_prior
-        cur.next = cur_next
+        in_prior = new_place
+        in_next = new_place.next
+
+        out_next.prior = out_prior
+        out_prior.next = out_next
+
+        in_prior.next = cur
+        cur.prior = in_prior
+        cur.next = in_next
+        in_next.prior = cur
+
+        for node in (out_prior, out_next, in_prior, in_next, cur, new_place):
+            if node.next.prior != node or node.prior.next != node:
+                raise RuntimeError("You've fucked up now. Now, you've fucked up.")
 
     print_array(zero)
+    print_array_backwards(zero)
 
-    moves = sorted([1000 % length, 2000 % length, 3000 % length])
-    progressive_moves = (moves[0], moves[1] - moves[0], moves[2] - moves[1])
+    moves = (1000, 2000, 3000)
     sum = 0
-    cur = zero
-    for steps in progressive_moves:
+    for steps in moves:
+        cur = zero
         for i in range(steps):
             cur = cur.next
         sum += cur.value
     print(sum)
 
+
 def print_array(zero):
     cur = zero
     arr = []
+    i = 0
     while True:
+        i += 1
         arr.append(cur.value)
         cur = cur.next
         if cur == zero:
             break
+        if i > 5002:
+            break
+    print(i)
+    print(arr)
 
+def print_array_backwards(zero):
+    cur = zero
+    arr = []
+    i = 0
+    while True:
+        i += 1
+        arr.append(cur.value)
+        cur = cur.prior
+        if cur == zero:
+            break
+        if i > 5002:
+            break
+    print(i)
+    print(arr)
 
 def main():
     part1()
